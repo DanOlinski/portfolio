7a08d301a4130a8a19be806132208a0bc0fafa81
Daniel Olinski Guterres <daniel.guterres@hotmail.com>
2026-03-14 12:20:03 -0400

    Auto-deploy: 2026-03-14 12:20:03

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import "./Resume.scss";
 import globalStates from '../hooks/globalStates';
 // this loads the image before the resume component is opened so that the animation of opening the resume component works smoothly
-import image from "../resume.jpg";
+import image from "../resume2026.jpg";
 
 export default function Footer() {
   const { resume, setResume } = globalStates()
@@ -14,6 +14,7 @@ export default function Footer() {
 
     if(resume){
       return(
+        <div className='outerContainer'>
       <div  className='resumeContainer'>
 
 <div ref={resumeRef}>
@@ -24,8 +25,8 @@ export default function Footer() {
         <div className='close close-stem2'></div>
         </div>
 
-        <a  href={`${process.env.PUBLIC_URL}/Daniel-Olinski-Resume.pdf`} 
-  download="Daniel-Olinski-Resume.pdf">
+        <a  href={`${process.env.PUBLIC_URL}/Daniel_Olinski-Resume.pdf`} 
+  download="Daniel_Olinski-Resume.pdf">
         
         <img className='downloadIcon' src="/portfolio/images/downloadIcon.png" alt=""></img>
 
@@ -37,6 +38,7 @@ export default function Footer() {
       <img className="resumePic" src={image} alt=""></img>
       </div>
 </div>
+      </div>
       </div>
       )
     }
